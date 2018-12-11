8ee498081c36bb470a0464facf0b8cb54008f9d4
Fabian <sabljak_fabian@web.de>
2018-12-11 13:40:10 +0100

    legendre calculation and saving to csv for all images implemented

--- features.py
+++ features.py
@@ -12,12 +12,23 @@ import matplotlib
 import matplotlib.pyplot as plt
 import main as m
 
+
 def show_image(img):
-# plot image together with grid
+    # plot image together with grid
     plt.imshow(img, cmap='gray', norm=matplotlib.colors.Normalize(vmin=0, vmax=200, clip=True))
-    #plt.xticks(np.arange(img.shape[1])-0.5)
-    #plt.yticks(np.arange(img.shape[0])-0.5)
+    # plt.xticks(np.arange(img.shape[1])-0.5)
+    # plt.yticks(np.arange(img.shape[0])-0.5)
     plt.grid(True, which='major')
+
+
+"""
+Moments
+- geometric_moment
+- central_moment
+- normalized_moment
+- hu_moment
+- legendre_moment
+"""
 
 
 # geometric moments
@@ -27,6 +38,7 @@ def geometric_moment(img, p, q):
         for x in range(img.shape[1]):
             s += x**p * y**q * img[y, x]
     return s
+
 
 # central moments
 def central_moment(img, p, q):
@@ -38,9 +50,11 @@ def central_moment(img, p, q):
             s += (x-x0)**p * (y-y0)**q * img[y, x]
     return s
 
+
 # normalised moments
 def normalized_moment(img, p, q):
     return central_moment(img, p, q) / (central_moment(img, 0, 0)**(1+((p+q))/2))
+
 
 # hu moments
 def hu_moment(img, i):
@@ -69,6 +83,7 @@ def hu_moment(img, i):
     else:
         return 0
 
+
 # legendre moments
 def lp(n, x):
     if n == 0:
@@ -78,14 +93,18 @@ def lp(n, x):
     else:
         return ((2*n-1)*x*lp(n-1, x) - (n-1)*lp(n-2, x)) / n
 
+
 def beta(img, m, n):
     return (2 * m + 1) * (2 * n + 1) / (img.shape[0] * img.shape[1])
+
 
 def xcoord(img, i):
     return ((2*i)/(img.shape[1]-1)) - 1
 
+
 def ycoord(img, j):
     return ((2*j)/(img.shape[0]-1)) - 1
+
 
 def legendre_moment(img, m, n):
     s = 0
@@ -95,19 +114,30 @@ def legendre_moment(img, m, n):
     return beta(img, m, n) * s
 
 
+def legendre_moments(info, img):
+    for color in "RGB":
+        for p in range(3):
+            for q in range(3):
+                info['Moment_{0}_L{1}{2}'.format(color, p, q)] = 'default'
+
+    for i in range(0, len(img)):
+        im_id = info['image_id'][i]
+        for p in range(3):
+            for q in range(3):
+                m = legendre_moment(img[i], p, q)
+                info.loc[info['image_id'] == im_id, ['Moment_R_L{0}{1}'.format(p, q)]] = m[0]
+                info.loc[info['image_id'] == im_id, ['Moment_G_L{0}{1}'.format(p, q)]] = m[1]
+                info.loc[info['image_id'] == im_id, ['Moment_B_L{0}{1}'.format(p, q)]] = m[2]
+
+        #print(info.loc[info['image_id'] == im_id])
+        info.to_csv('featuresss.csv')
 
 
 if __name__ == '__main__':
-    images, labels = m.load_data()
+    _, images, labels = m.load_data()
     img = images[0]
     show_image(img)
 
-    for idx, img in enumerate([img]):
-        print(f'Image {idx + 1}:')
-        for p in range(3):
-            for q in range(3):
-                m = legendre_moment(img, p, q)
-                print(f'L_{p}{q} = {m}')
-        print()
 
-    x = 0
+    m = legendre_moments(img)
+    print(m)
--- main.py
+++ main.py
@@ -9,9 +9,10 @@ Created on Sun Dez 2 01:23:34 2018
 
 import numpy as np
 import pandas as pd
-from PIL import Image
+from PIL import Image, ImageOps
 from skimage import io, exposure
 from matplotlib import pyplot as plt
+from features import *
 
 #############################################
 # Parameters                                #
@@ -24,31 +25,31 @@ def load_data():
     labels = []
 
     # open .csv file with image names and labels
-    infos = pd.read_csv(im_path + 'labels.csv')
+    infos = pd.read_csv(im_path + 'labels.csv').sort_values('image_id')
     # image names
     im_ids = infos.image_id
     # number of images
     im_num = im_ids.size
 
-    # open images, then save to list of numpy arrays
-    for i in range(0, 3):
+    # open images, equalize histogram, then save to list of numpy arrays
+    for i in range(0, im_num):
         im = Image.open(im_path + im_ids[i] + '.jpg')
         im.load()
-        # histogram equalisation (and array transformation)
-        im_hist = exposure.equalize_hist(np.asarray(im, dtype='int32'))
-        # append to list
-        images.append(im_hist)
 
-        plt.imshow(im_hist)
+        im_hist = ImageOps.equalize(im)
+        # append to list
+        images.append(np.asanyarray(im_hist))
 
     # get labels
     labels = infos.dx
-    
-    return images, labels
 
-
+    return infos, images, labels
 
 
 if __name__ == '__main__':
-    images, labels = load_data()
-    x = 0
+    info, images, labels = load_data()
+
+    # it will take forever to calculate the moments
+    # I calculate them for all images and provide them in a csv file
+    legendre_moments(info, images)
+
